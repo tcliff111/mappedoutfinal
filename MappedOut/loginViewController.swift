d8f3daa019d6e11fa2fc39096e9cb8904e32f7dc
lvlingsheng <lvlingsheng@gmail.com>
2016-03-08 18:40:22 -0600

    Version 0.1
    Test

---
+++ MappedOut/loginViewController.swift
@@ -0,0 +1,36 @@
+//
+//  loginViewController.swift
+//  MappedOut
+//
+//  Created by 吕凌晟 on 16/3/8.
+//  Copyright © 2016年 Codepath. All rights reserved.
+//
+
+import UIKit
+import Parse
+
+class loginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
